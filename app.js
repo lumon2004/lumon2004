acc44abf9226b70b8de16758098a6b37d63a6899
Luca <97289417+lumon2004@users.noreply.github.com>
2022-10-31 16:00:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function openMenu() {
+    if (document.body.offsetWidth >= 415) {
+        document.getElementById("sideMenu").style.width="20%";
+        document.getElementById("sideMenu").style.marginRight="80%";
+    } else if (document.body.offsetWidth <= 414) {
+        document.getElementById("sideMenu").style.width="100%";
+        document.getElementById("sideMenu").style.marginRight="0%";
+    }
+};
+function closeMenu() {
+    document.getElementById("sideMenu").style.width="0%";
+    document.getElementById("sideMenu").style.marginRight="100%";
+};
