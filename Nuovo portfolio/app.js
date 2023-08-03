0877be7608f1786382ce5f857496512794afbce4
Luca <lucamonaco04@libero.it>
2023-08-03 19:11:31 +0200

    Work in progress for new Portfolio

--- Nuovo portfolio/app.js
+++ Nuovo portfolio/app.js
@@ -1,7 +1,69 @@
+/* APRI E CHIUDI HEADER MENU */
+
 function openMobileMenu() {
     document.getElementsByClassName('mobileMenu')[0].style.display="block";
 }
 
 function closeMobileMenu() {
     document.getElementsByClassName('mobileMenu')[0].style.display="none";
+}
+
+/* APRI E CHIUDI MENU LATERALE INTERNO HEADER MENU */
+
+function openSideMenu(i, j) {
+    document.getElementsByClassName('sideMenu')[i].style.display="block";
+    if (i==0 && j==22) {
+        for (j=j+1; j<=51; j++) {
+            if (!(j>=24 && j<=34)) {
+                document.getElementsByTagName('li')[j].style.display='none';
+            }
+        }
+    } else if (i==1 && j==35) {
+        document.getElementsByTagName('li')[23].style.display='none';
+        document.getElementsByTagName('li')[35].style.display='none';
+        for (j=j+1; j<=51; j++) {
+            if ((!(j>=37 && j<=43))) {
+                document.getElementsByTagName('li')[j].style.display='none';
+            }
+        }
+    } else if (i==2 && j==44) {
+        document.getElementsByTagName('li')[23].style.display='none';
+        document.getElementsByTagName('li')[35].style.display='none';
+        document.getElementsByTagName('li')[36].style.display='none';
+        document.getElementsByTagName('li')[44].style.display='none';
+        for (j=j+1; j<=51; j++) {
+            if ((!(j>=46 && j<=51))) {
+                document.getElementsByTagName('li')[j].style.display='none';
+            }
+        }
+    }
+}
+
+function closeSideMenu(i, j) {
+    document.getElementsByClassName('sideMenu')[i].style.display="none";
+    if (i==0 && j==22) {
+        for (j=j+1; j<=51; j++) {
+            if (!(j>=24 && j<=34)) {
+                document.getElementsByTagName('li')[j].style.display='flex';
+            }
+        }
+    } else if (i==1 && j==35) {
+        document.getElementsByTagName('li')[23].style.display='flex';
+        document.getElementsByTagName('li')[35].style.display='flex';
+        for (j=j+1; j<=51; j++) {
+            if ((!(j>=37 && j<=43))) {
+                document.getElementsByTagName('li')[j].style.display='flex';
+            }
+        }
+    } else if (i==2 && j==44) {
+        document.getElementsByTagName('li')[23].style.display='flex';
+        document.getElementsByTagName('li')[35].style.display='flex';
+        document.getElementsByTagName('li')[36].style.display='flex';
+        document.getElementsByTagName('li')[44].style.display='flex';
+        for (j=j+1; j<=51; j++) {
+            if ((!(j>=46 && j<=51))) {
+                document.getElementsByTagName('li')[j].style.display='flex';
+            }
+        }
+    }
 }
